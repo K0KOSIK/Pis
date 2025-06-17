cbe56ffec0651f598b8bd826fdee7d7479aa0af0
К0КОСИК <zlodanon@gmail.com>
2025-06-17 18:19:24 +0400

    -0

--- ConnectionSelectorForm.Designer.cs
+++ ConnectionSelectorForm.Designer.cs
@@ -32,6 +32,7 @@
             logs = new Label();
             btnConnect = new Button();
             SMS = new Label();
+            panel1 = new Panel();
             SuspendLayout();
             // 
             // cmbConnections
@@ -83,12 +84,20 @@
             SMS.TabIndex = 11;
             SMS.Text = "SMS";
             // 
+            // panel1
+            // 
+            panel1.Location = new Point(587, 230);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 100);
+            panel1.TabIndex = 13;
+            // 
             // ConnectionSelectorForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
             Controls.Add(btnConnect);
             Controls.Add(SMS);
             Controls.Add(cmbConnections);
@@ -106,5 +115,6 @@
         private Label logs;
         private Button btnConnect;
         private Label SMS;
+        private Panel panel1;
     }
 }
